d15d0c16b62b72282449fe66238d6db85c06c191
BryanRoth <bryan.roth@hypertherm.com>
2022-03-02 13:32:24 -0500

    Fix: Test RegionWorksWhenContentViewIsTopChild is failing
    Changes to support a more WPF region like boot experience are exposing how the standard Prism.Forms and Region Navigation are not merged well. I.e. they have significantly different "navigation" side effects (on view/viewmodel interfaces). This case exposes issues during app startup where the IInitialize interface wasn't being exercised. There are others, this just gets this test back online. More to look into here.

---
+++ tests/Forms/Prism.DI.Forms.Tests/Mocks/PrismApplicationRegionMock.cs
@@ -0,0 +1,27 @@
+﻿using Prism.Ioc;
+using Xamarin.Forms;
+
+namespace Prism.DI.Forms.Tests
+{
+    public class PrismApplicationRegionMock<T> : PrismApplicationMock where T : Page
+    {
+        public PrismApplicationRegionMock( IPlatformInitializer platformInitializer )
+            : base( platformInitializer )
+        {}
+
+        protected override void RegisterTypes( IContainerRegistry containerRegistry )
+        {
+            containerRegistry.RegisterRegionServices();
+        }
+
+#region Overrides of PrismApplicationBase
+
+        /// <inheritdoc />
+        protected override Page CreateShell()
+        {
+            return Container.Resolve<T>();
+        }
+
+#endregion
+    }
+}
